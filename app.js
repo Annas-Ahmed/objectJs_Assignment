fb40ded641c92ea13294422f1ec28f8521a4c25d
Annas-Ahmed <annasahmed1609@gmail.com>
2022-06-04 12:44:37 +0500

    tastk comolete

---
+++ app.js
@@ -0,0 +1,60 @@
+// question1
+
+// var itemsArray = [
+//     { name: "juice", price: "50", quantity: "3" },
+//     { name: "cookie", price: "30", quantity: "9" },
+//     { name: "shirt", price: "880", quantity: "1" },
+//     { name: "pen", price: "100", quantity: "2" }];
+
+// function totalPrice(i){
+    
+//     // console.log(parseInt(itemsArray[i].price))
+//     // console.log(parseInt(itemsArray[i].quantity))
+
+//     price=parseInt(itemsArray[i].price)*parseInt(itemsArray[i].quantity)
+//     console.log(itemsArray[i].name+"price=",price)
+// }
+// totalPrice(0)
+// totalPrice(1)
+// totalPrice(2)
+// totalPrice(3)
+
+
+// question2
+// var obj={
+//     name:"annas",
+//     email:"annas@gmail.co",
+//     password:"pass1",
+//     age:"20",
+//     gender:"male",
+//     city:"Karachi",
+//     country:"PAkistan"
+// }
+
+// console.log(obj.hasOwnProperty('age'))
+// console.log(obj.hasOwnProperty('country'))
+// console.log(obj.hasOwnProperty('firstName'))
+// console.log(obj.hasOwnProperty('lastName'))
+
+// question3
+// function Obj(name,email,password){
+//     this.name=name
+//     this.email=email
+//     this.passwordpassword
+// }
+
+// std1=Obj("annas","ann@gmail.com","pass12")
+// std2=Obj("annas1","an1n@gmail.com","pa1ss12")
+
+// function Pop(name, address){
+//     this.name=name
+//     this.gender=gender
+//     this.address=address
+//     this.education=education
+//     this.profession=profession
+// }
+
+// var record=Pop()
+
+// input=document.getElementsByTagName("input")
+// Pop(input[0],input[1])
